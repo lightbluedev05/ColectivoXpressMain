596fdc02f85ea01d003c0592bd8158c23cae4c58
Roberto Pizarro <roberto.pizarrod@unmsm.edu.pe>
2024-11-12 18:06:52 -0500

    PerfilPasajero.java para el DashBoardPasajero Metodos para actualizar datos en Pasajero.java

--- src/Models/Pasajero.java
+++ src/Models/Pasajero.java
@@ -40,7 +40,27 @@ public class Pasajero extends Usuario {
         this.contrasena = nueva_contrasena;
         return new PasajeroRepository().actualizar(this);
     }
-
+    
+    public boolean actualizar_nombre(String nueva_nombre) {
+        this.nombre = nueva_nombre;
+        return new PasajeroRepository().actualizar(this);
+    }
+    
+    public boolean actualizar_distrito(String nueva_distrito) {
+        this.distrito = nueva_distrito;
+        return new PasajeroRepository().actualizar(this);
+    }
+    
+    public boolean actualizar_provincia(String nueva_provincia) {
+        this.provincia = nueva_provincia;
+        return new PasajeroRepository().actualizar(this);
+    }
+    
+    public boolean actualizar_departamento(String nueva_departamento) {
+        this.departamento = nueva_departamento;
+        return new PasajeroRepository().actualizar(this);
+    }
+    
     public static boolean registro_pasajero(Pasajero nuevo_pasajero){
         return new PasajeroRepository().crear(nuevo_pasajero);
     }
